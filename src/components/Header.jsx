b5e6576dda32270a745e03072fdacf73ea889535
mithun20522 <mithun20522@iiitd.ac.in>
2024-01-06 17:29:30 +0530

    Created server for backend succesfully.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react'
 import { Link } from "react-router-dom"
+import {IoMenu} from "react-icons/io5"
+import {IoMdClose} from "react-icons/io"
 
 const Header = () => {
+const [isClosed, setIsClosed]  = useState(false)
   return (
     <div className='bg-slate-200'>
         <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
-           <Link to='/'><h1 className="font-bold text-3xl">Auth</h1></Link> 
-            <ul className='flex gap-4 bg-red-600'>
+            <Link to='/'><h1 className="font-bold text-3xl">Auth</h1></Link> 
+            <ul className='flex gap-4'>
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/login'>Login</Link>
             </ul>
-            
         </div>
+        
     </div>
   )
 }
